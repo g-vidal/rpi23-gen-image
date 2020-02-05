47ebd0c006949b36ce59955aa87ab3a0854902d5
g-vidal <gerard.vidal@ens-lyon.fr>
2020-02-05 13:53:22 +0100

    Still errors on 11-apt.sh

--- bootstrap.d/11-apt.sh
+++ bootstrap.d/11-apt.sh
@@ -11,21 +11,8 @@ if [ -z "$APT_PROXY" ] ; then
   sed -i "s/\"\"/\"${APT_PROXY}\"/" "${ETC_DIR}/apt/apt.conf.d/10proxy"
 fi
 
-if [ "$BUILD_KERNEL" = false ] ; then
-  # Install APT pinning configuration for flash-kernel package
-  install_readonly files/apt/flash-kernel "${ETC_DIR}/apt/preferences.d/flash-kernel"
-
-  # Install APT sources.list
-  install_readonly files/apt/sources.list "${ETC_DIR}/apt/sources.list"
-  echo "deb ${COLLABORA_URL} ${RELEASE} rpi2" >> "${ETC_DIR}/apt/sources.list"
-
-  # Upgrade collabora package index and install collabora keyring
-  chroot_exec apt-get -qq -y update
-  # Removed --allow-unauthenticated as suggested after modification on _apt privileges
-  chroot_exec apt-get -qq -y install collabora-obs-archive-keyring
-else # BUILD_KERNEL=true
-  # Install APT sources.list
-  install_readonly files/apt/sources.list "${ETC_DIR}/apt/sources.list"
+# Install APT sources.list
+install_readonly files/apt/sources.list "${ETC_DIR}/apt/sources.list"
 
 # Use specified APT server and release
 sed -i "s/\/ftp.debian.org\//\/${APT_SERVER}\//" "${ETC_DIR}/apt/sources.list"
